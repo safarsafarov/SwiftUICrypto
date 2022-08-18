0d412037a179a8c61f2a44e5f3776a37054c3e2b
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-18 12:24:47 +0500

    ✨

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 class HomeViewModel: ObservableObject {
-    @Published var allCoincs: [CoinModel] = []
+    @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
     init() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoincs.append(DeveloperPreview.instance.coin)
+            self.allCoins.append(DeveloperPreview.instance.coin)
             self.portfolioCoins.append(DeveloperPreview.instance.coin)
         }
         
