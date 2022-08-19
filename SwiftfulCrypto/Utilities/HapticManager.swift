ef9e4665c416dbf96c060839552cd6babd83c3c0
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-19 16:31:58 +0500

    ✨

---
+++ SwiftfulCrypto/Utilities/HapticManager.swift
@@ -0,0 +1,12 @@
+import Foundation
+import SwiftUI
+
+class HapticManager {
+    
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        generator.notificationOccurred(type)
+    }
+    
+}
